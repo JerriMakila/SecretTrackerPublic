b70aa43d9c5b4f1c160a20828ac5fe70ebc1862b
Jerri Mäkilä <jerri.makila@gmail.com>
2021-09-22 11:51:23 +0300

    Project initialized

---
+++ src/main/java/fi/palvelinohjelmointi/secrettracker/SecrettrackerApplication.java
@@ -0,0 +1,13 @@
+package fi.palvelinohjelmointi.secrettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecrettrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecrettrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/palvelinohjelmointi/secrettracker/SecrettrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.palvelinohjelmointi.secrettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecrettrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
