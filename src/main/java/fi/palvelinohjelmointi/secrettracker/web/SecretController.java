b0db97e938b620dd987719f5ea627f29398f0398
Jerri Mäkilä <jerri.makila@gmail.com>
2021-09-22 21:24:29 +0300

    SecretController produces XML instead of JSON

---
+++ src/main/java/fi/palvelinohjelmointi/secrettracker/web/SecretController.java
@@ -0,0 +1,29 @@
+package fi.palvelinohjelmointi.secrettracker.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fi.palvelinohjelmointi.secrettracker.domain.Secret;
+import fi.palvelinohjelmointi.secrettracker.domain.SecretRepository;
+
+@RestController
+public class SecretController {
+	@Autowired
+	private SecretRepository secretRepository;
+	
+	@GetMapping("/secrets")
+	public @ResponseBody List<Secret> secrets(){
+		return (List<Secret>) secretRepository.findAll();
+	}
+	
+	@GetMapping("/secrets/{id}")
+	public @ResponseBody Optional<Secret> getSecretById(@PathVariable("id") Long secretId){
+		return secretRepository.findById(secretId);
+	}
+}
