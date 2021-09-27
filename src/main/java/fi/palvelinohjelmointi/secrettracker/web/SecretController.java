2bf26b10f1f5b9574c3e921a1953039899a16d6a
Jerri Mäkilä <jerri.makila@gmail.com>
2021-09-27 19:27:08 +0300

    Korjailtu yksittäisen secretin get-pyyntöä

--- src/main/java/fi/palvelinohjelmointi/secrettracker/web/SecretController.java
+++ src/main/java/fi/palvelinohjelmointi/secrettracker/web/SecretController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,7 +25,13 @@ public class SecretController {
 	}
 	
 	@GetMapping("/secret/{id}")
-	public @ResponseBody Optional<Secret> getSecretById(@PathVariable("id") Long secretId){
-		return secretRepository.findById(secretId);
+	public @ResponseBody ResponseEntity<Optional<Secret>> getSecretById(@PathVariable("id") Long secretId){
+		Optional<Secret> secret = secretRepository.findById(secretId);
+		
+		if(secret.isEmpty()) {
+			return new ResponseEntity<>(secret, HttpStatus.NOT_FOUND);
+		}else {
+			return new ResponseEntity<>(secret, HttpStatus.OK);
+		}
 	}
 }
